6634a3b93c875034fe56b55ceb0b2ebd3396b808
nfajibfhv <156654263+nfajibfhv@users.noreply.github.com>
2024-05-29 12:14:12 +0200

    Create FoldUtils.java

---
+++ sdk/src/java/org/lineageos/internal/util/FoldUtils.java
@@ -0,0 +1,16 @@
+package org.lineageos.internal.util;
+
+import java.lang.System;
+
+public enum FoldVariant {
+    VARIANT_FLAT,
+    VARIANT_FLIP,
+    VARIANT_FOLD;
+
+    private static final String FOLD_VARIANT_SYSTEM_PROPERTY = ro.lineageos.foldvariant
+
+    public static FoldVariant getFoldVariant {
+    switch ((int) System.getProperty(FOLD_VARIANT_SYSTEM_PROPERTY)
+             case 
+    }
+}
