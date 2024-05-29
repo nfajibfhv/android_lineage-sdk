afcd4a783e253c231aa2a8cb8565ccacad005085
nfajibfhv <156654263+nfajibfhv@users.noreply.github.com>
2024-05-29 14:17:14 +0200

    Update FoldUtils.java

--- sdk/src/java/org/lineageos/internal/util/FoldUtils.java
+++ sdk/src/java/org/lineageos/internal/util/FoldUtils.java
@@ -7,10 +7,15 @@ public enum FoldVariant {
     VARIANT_FLIP,
     VARIANT_FOLD;
 
-    private static final String FOLD_VARIANT_SYSTEM_PROPERTY = ro.lineageos.foldvariant
+    private static final String FOLD_VARIANT_SYSTEM_PROPERTY = "ro.lineageos.foldvariant"
 
     public static FoldVariant getFoldVariant {
-    switch ((int) System.getProperty(FOLD_VARIANT_SYSTEM_PROPERTY)
-             case 
+        switch ((int) System.getProperty(FOLD_VARIANT_SYSTEM_PROPERTY)
+                case 0:
+                    return VARIANT_FLAT;
+                case 1:
+                    return VARIANT_FLIP;
+                case 2: 
+                    return VARIANT_FOLD;
     }
 }
